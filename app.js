603fc2b583afe6cf58626b2c63cad59a60ec7738
Burda Eugene <burda.brabus@gmail.com>
2022-04-22 19:46:28 +0300

    (fix) try to fix error 503 on heroku server try 3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 require("./routes")(app);
 // mongoose
