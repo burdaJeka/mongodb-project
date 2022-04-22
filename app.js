2194ccfc1378679f9c1b4e59b464cfc32aaafc18
Burda Eugene <burda.brabus@gmail.com>
2022-04-22 16:45:45 +0300

    (fix) try to fix error 503 on heroku server

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const DB_HOST = require("./config.js");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const PORT = 3000;
 
 require("./routes")(app);
-mongoose
-  .connect(DB_HOST, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Database connection success !");
-  })
-  .catch((error) => {
-    console.log(error.message);
-  });
+// mongoose
+//   .connect(DB_HOST, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Database connection success !");
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//   });
 
 app.listen(PORT, () => {
   console.log(`Server started... \n${new Date()} \nServer port is : ${PORT}`);
